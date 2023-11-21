a3bc0b3a1b7877edb7152fa4b72e87973cc86849
Anne-Claire <116877661+anclko@users.noreply.github.com>
2023-11-21 16:40:21 +0100

    Update AlgorithmRunner.java
    final changes

--- src/AlgorithmRunner.java
+++ src/AlgorithmRunner.java
@@ -12,12 +12,34 @@ public class AlgorithmRunner {
         int warmupIterations = 5;
         WarmUp.warmUp(numbers, algorithm, warmupIterations);
 
-        // Running the program 10 times after warm up
-        long totalElapsedTime = 0;
+        // Time Set Up For Each Algorithm:
+        long totalInsertionIter= 0;
+        long totalInsertionRec = 0;
+        long totalRandomIter = 0;
+        long totalRandomRec = 0;
+        long totalMedianIter = 0;
+        long totalMedianRec = 0;
+        long totalArr0Iter = 0;
+        long totalArr0Rec = 0;
+        long totalBinary = 0;
+        long totalBinary2 = 0;
+
+        // Average Time Algorithm
+        double averageII = 0;
+        double averageIR = 0;
+        double averageRI = 0;
+        double averageRR = 0;
+        double averageMI = 0;
+        double averageMR = 0;
+        double averageFI = 0;
+        double averageFR = 0;
+        double averageB = 0;
+        double averageB2 = 0;
 
         int[] array = new int[0];
         int[] arrayForBinarySearch = new int[0];
         int resultIndex = -1;
+        int resultIndex2 = -1;
 
         for (int run = 0; run < numberOfRuns; run++) {
             // Create a list to store the numbers
@@ -27,61 +49,120 @@ public class AlgorithmRunner {
             // Sort the list for Binary search algorithm only
             Arrays.sort(arrayForBinarySearch);
 
-            // start countdown
-            long startTime = System.nanoTime();
-
             // Call algorithms to run
             switch (algorithm) {
                 case "Insertion Iterative":
+                    long startII = System.nanoTime();
                     insertionIterative.insertionSort(array);
+                    long endTimeII = System.nanoTime();
+                    totalInsertionIter += (endTimeII - startII);
+                    averageII = (double) (totalInsertionIter / numberOfRuns) /1000;
                     break;
                 case "Insertion Recursive":
+                    long startIR = System.nanoTime();
                     insertionRecursive.insertionSortRecursive(array, 1, array.length - 1);
+                    long endIR = System.nanoTime();
+                    totalInsertionRec += (endIR - startIR);
+                    averageIR = (double) (totalInsertionRec / numberOfRuns)/1000;
                     break;
                 case "QS Random Pivot Iterative":
-                    randomIterative.quickSortRandomIterative(array, 0, array.length - 1);
+                    long startRI = System.nanoTime();
+                    randomQuicksort.quickSortRandomIterative(array);
+                    long endRI = System.nanoTime();
+                    totalRandomIter += (endRI - startRI);
+                    averageRI = (double) (totalRandomIter/ numberOfRuns)/1000;
                     break;
                 case "QS Random Pivot Recursive":
-                    randomRecursive.quickSortRandomRecursive(array, 0, array.length - 1);
+                    long startRR = System.nanoTime();
+                    randomQuicksort.quickSortRandomRecursive(array, 0, array.length - 1);
+                    long endRR = System.nanoTime();
+                    totalRandomRec += (endRR - startRR);
+                    averageRR = (double) (totalRandomRec/ numberOfRuns)/1000;
                     break;
                 case "QS Array[0] Pivot Iterative":
-                    firstElementIterative.quickSortIterative(array, 0, array.length - 1);
+                    long startFI = System.nanoTime();
+                    firstElementIterative.firstIterative(array);
+                    long endFI = System.nanoTime();
+                    totalArr0Iter += (endFI - startFI);
+                    averageFI = (double) (totalArr0Iter/ numberOfRuns)/1000;
                     break;
                 case "QS Array[0] Pivot Recursive":
-                    firstElementRecursive.quickSortRecursive(array, 0, array.length - 1);
+                    long startFR = System.nanoTime();
+                    firstElementRecursive.firstRecursive(array, 0, array.length - 1);
+                    long endFR = System.nanoTime();
+                    totalArr0Rec += (endFR - startFR);
+                    averageFR = (double) (totalArr0Rec/ numberOfRuns)/1000;
                     break;
                 case "QS Median Pivot Iterative":
-                    medianIterative.quickSortMedianIterative(array, 0, array.length - 1);
+                    long startMI = System.nanoTime();
+                    medianIterative.medianIter(array);
+                    long endMI = System.nanoTime();
+                    totalMedianIter += (endMI - startMI);
+                    averageMI = (double) (totalMedianIter/ numberOfRuns)/1000;
                     break;
                 case "QS Median Pivot Recursive":
-                    medianRecursive.quickSortMedianRecursive(array, 0, array.length - 1);
+                    long startMR = System.nanoTime();
+                    medianRecursive.medianRec(array, 0, array.length - 1);
+                    long endMR = System.nanoTime();
+                    totalMedianRec += (endMR - startMR);
+                    averageMR = (double) (totalMedianRec/ numberOfRuns)/1000;
                     break;
                 case "Binary Search":
-                    resultIndex = binarySearch.binarySearchSort(arrayForBinarySearch, 49, 0, arrayForBinarySearch.length -1);
+                    long startB = System.nanoTime();
+                    resultIndex = binarySearch.binarySearchSort(arrayForBinarySearch, 80, 0, arrayForBinarySearch.length - 1);
+                    long endB = System.nanoTime();
+                    totalBinary += (endB - startB);
+                    averageB = (double) (totalBinary/ numberOfRuns)/1000;
+
+                    long startB2 = System.nanoTime();
+                    resultIndex2 = binarySearch.binarySearchSort(arrayForBinarySearch, 120, 0, arrayForBinarySearch.length - 1);
+                    long endB2 = System.nanoTime();
+                    totalBinary2 += (endB2 - startB2);
+                    averageB2 = (double) (totalBinary2/ numberOfRuns)/1000;
+
             }
-
-            // Measure the end time
-            long endTime = System.nanoTime();
-
-            //end time calculated to nanoseconds
-            totalElapsedTime += (endTime - startTime);
-
         }
-
-        // average time calculation
-        double averageElapsedTimeMicro = (double) (totalElapsedTime / numberOfRuns)/1000;
-        double averageElapsedTimeMs = averageElapsedTimeMicro/1000;
 
         // print results in formatted way
         if ("Binary Search".equals(algorithm)) {
-            ResultFormatter.printResults(algorithm, size, averageElapsedTimeMs, arrayForBinarySearch, numberOfElementsToShow);
+            ResultFormatter.printResults(algorithm, size, averageB, arrayForBinarySearch, numberOfElementsToShow);
             if (resultIndex != -1) {
-                System.out.println("Target value found.");
+                System.out.println("Target found at index: " + resultIndex);
             } else {
-                System.out.println("Target value not found in the array.");
+                System.out.println("Target not found");
             }
-        } else {
-            ResultFormatter.printResults(algorithm, size, averageElapsedTimeMs, array, numberOfElementsToShow);
+        }
+        if ("Binary Search".equals(algorithm)) {
+            ResultFormatter.printResults(algorithm, size, averageB2, arrayForBinarySearch, numberOfElementsToShow);
+            if (resultIndex2 != -1) {
+                System.out.println("Target found at index: " + resultIndex);
+            } else {
+                System.out.println("Target not found");
+            }
+        }
+        if ("Insertion Iterative".equals(algorithm)) {
+            ResultFormatter.printResults(algorithm, size, averageII, array, numberOfElementsToShow);
+        }
+        if ("Insertion Recursive".equals(algorithm)) {
+            ResultFormatter.printResults(algorithm, size, averageIR, array, numberOfElementsToShow);
+        }
+        if ("QS Random Pivot Iterative".equals(algorithm)) {
+            ResultFormatter.printResults(algorithm, size, averageRI, array, numberOfElementsToShow);
+        }
+        if ("QS Random Pivot Recursive".equals(algorithm)) {
+            ResultFormatter.printResults(algorithm, size, averageRR, array, numberOfElementsToShow);
+        }
+        if ("QS Array[0] Pivot Iterative".equals(algorithm)) {
+            ResultFormatter.printResults(algorithm, size, averageFI, array, numberOfElementsToShow);
+        }
+        if ("QS Array[0] Pivot Recursive".equals(algorithm)) {
+            ResultFormatter.printResults(algorithm, size, averageFR, array, numberOfElementsToShow);
+        }
+        if ("QS Median Pivot Iterative".equals(algorithm)) {
+            ResultFormatter.printResults(algorithm, size, averageMI, array, numberOfElementsToShow);
+        }
+        if ("QS Median Pivot Recursive".equals(algorithm)) {
+            ResultFormatter.printResults(algorithm, size, averageMR, array, numberOfElementsToShow);
         }
     }
 }
