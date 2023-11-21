cdc4ca859d072962296ec330c745e356177893cd
Anne-Claire <116877661+anclko@users.noreply.github.com>
2023-11-21 16:43:41 +0100

    Update medianRecursive.java
    final changes

--- src/Algorithms/medianRecursive.java
+++ src/Algorithms/medianRecursive.java
@@ -1,57 +1,48 @@
 package Algorithms;
 
 public class medianRecursive {
-    static int medianOfThree(int[] arr, int low, int high) {
-        int mid = low + (high - low) / 2;
+    public static void medianRec(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = medianOfThreePivot(arr, low, high);
+            pivotIndex = partition(arr, low, high, pivotIndex);
+            medianRec(arr, low, pivotIndex - 1);
+            medianRec(arr, pivotIndex + 1, high);
+        }
+    }
 
-        if (arr[low] > arr[mid]) {
-            swap(arr, low, mid);
+    private static int medianOfThreePivot(int[] arr, int low, int high) {
+        int middle = low + (high - low) / 2;
+        if (arr[low] > arr[middle]) {
+            swap(arr, low, middle);
         }
         if (arr[low] > arr[high]) {
             swap(arr, low, high);
         }
-        if (arr[mid] > arr[high]) {
-            swap(arr, mid, high);
+        if (arr[middle] > arr[high]) {
+            swap(arr, middle, high);
         }
 
-        return mid;
+        // Move pivot to the end
+        swap(arr, middle, high - 1);
+        return high; // Use high - 1 as the pivot index
     }
 
-    static int partition(int[] arr, int low, int high) {
-        int pivotIndex = medianOfThree(arr, low, high);
+    private static int partition(int[] arr, int low, int high, int pivotIndex) {
         int pivot = arr[pivotIndex];
-
-        // Swap the pivot with the last element
-        swap(arr, pivotIndex, high);
-
-        int i = low;
+        int i = low - 1;
 
         for (int j = low; j < high; j++) {
             if (arr[j] <= pivot) {
-                swap(arr, i, j);
                 i++;
+                swap(arr, i, j);
             }
         }
 
-        // Swap the pivot to its final position
-        swap(arr, i, high);
-
-        return i;
+        swap(arr, i + 1, high);
+        return i + 1;
     }
 
-    // Recursive quicksort function with median of three pivot
-    public static void quickSortMedianRecursive(int[] arr, int low, int high) {
-        if (low < high) {
-            // Set pivot element at its correct position in the sorted array
-            int p = partition(arr, low, high);
-
-            // Recursively sort elements before and after the partition
-            quickSortMedianRecursive(arr, low, p - 1);
-            quickSortMedianRecursive(arr, p + 1, high);
-        }
-    }
-
-    static void swap(int[] arr, int i, int j) {
+    private static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
