0f713215194e1a69d011a7456bb89ae890a94f8c
Anne-Claire <116877661+anclko@users.noreply.github.com>
2023-11-21 16:41:55 +0100

    Update binarySearch.java
    final changes

--- src/Algorithms/binarySearch.java
+++ src/Algorithms/binarySearch.java
@@ -4,20 +4,19 @@ public class binarySearch {
 
     public static int binarySearchSort(int[] numbers, int numberToFind, int low, int high) {
 
-        if (high >= low && low <= numbers.length - 1) {
-            int middlePosition = low + (high - low) / 2;
-            int middleNumber = numbers[middlePosition];
-
-            if (numberToFind == middleNumber){
-                return middlePosition;
-            }
-            if (numberToFind < middleNumber){
-                return binarySearchSort(numbers, numberToFind, low, middlePosition - 1);
-            }
-            else {
-                return binarySearchSort(numbers, numberToFind, middlePosition + 1, high);
-            }
+        if (numbers == null || low > high) {
+            return -1; // Invalid input or element not found
         }
-        return -1;
+
+        int middlePosition = low + (high - low) / 2;
+        int middleNumber = numbers[middlePosition];
+
+        if (numberToFind == middleNumber) {
+            return middlePosition;
+        } else if (numberToFind < middleNumber) {
+            return binarySearchSort(numbers, numberToFind, low, middlePosition - 1);
+        } else {
+            return binarySearchSort(numbers, numberToFind, middlePosition + 1, high);
+        }
     }
 }
