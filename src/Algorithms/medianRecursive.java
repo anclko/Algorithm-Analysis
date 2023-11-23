7eaf8944895e17b79697cfc3ceeb89306b048178
Anne-Claire <116877661+anclko@users.noreply.github.com>
2023-11-23 23:04:47 +0100

    Update medianRecursive.java

--- src/Algorithms/medianRecursive.java
+++ src/Algorithms/medianRecursive.java
@@ -1,49 +1,68 @@
 package Algorithms;
 
-public class medianRecursive {
-    public static void medianRec(int[] arr, int low, int high) {
+
+import java.util.Random;
+import java.util.Stack;
+
+public class randomQuicksort {
+
+    private static final Random rand = new Random();
+
+    public static void randomIterative(int[] arr, int low, int high) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(low);
+        stack.push(high);
+
+        while (!stack.isEmpty()) {
+            high = stack.pop();
+            low = stack.pop();
+
+            if (low < high) {
+                int pivotIndex = randomPivot(low, high);
+                int pivotFinalIndex = partition(arr, low, high, pivotIndex);
+
+                // Push sub-arrays to the stack for later processing
+                if (pivotFinalIndex - 1 > low) {
+                    stack.push(low);
+                    stack.push(pivotFinalIndex - 1);
+                }
+                if (pivotFinalIndex + 1 < high) {
+                    stack.push(pivotFinalIndex + 1);
+                    stack.push(high);
+                }
+            }
+        }
+    }
+
+    public static void randomRecursive(int[] arr, int low, int high) {
         if (low < high) {
-            int pivotIndex = medianOfThree(arr, low, high);
+            int pivotIndex = randomPivot(low, high);
             int pivotFinalIndex = partition(arr, low, high, pivotIndex);
 
             // Check if pivotFinalIndex is within bounds
             if (pivotFinalIndex > low && pivotFinalIndex < high) {
                 // Recursive call on the left of pivot
-                medianRec(arr, low, pivotFinalIndex - 1);
+                randomRecursive(arr, low, pivotFinalIndex - 1);
 
                 // Recursive call on the right of pivot
-                medianRec(arr, pivotFinalIndex + 1, high);
+                randomRecursive(arr, pivotFinalIndex + 1, high);
             }
         }
     }
 
-    public static int medianOfThree(int[] arr, int low, int high) {
-        int mid = low + (high - low) / 2;
-
-        // Sort the indices to find the median
-        if (arr[low] > arr[mid]) {
-            swap(arr, low, mid);
-        }
-        if (arr[mid] > arr[high]) {
-            swap(arr, mid, high);
-        }
-        if (arr[low] > arr[mid]) {
-            swap(arr, low, mid);
-        }
-
-        // The median of three is now at the midIndex
-        return mid;
+    public static int randomPivot(int low, int high) {
+        return rand.nextInt(high - low + 1) + low;
     }
 
     public static int partition(int[] arr, int low, int high, int pivotIndex) {
-        // Choose the pivot as the median of three
+        // Choose the pivot as the randomly generated index
         int pivot = arr[pivotIndex];
 
         // Swap the pivot element with the last element
-        swap(arr, pivotIndex, high - 1);
+        swap(arr, pivotIndex, high);
 
         int i = low - 1;
-        int j = high - 1;
+        int j = high;
 
         while (true) {
             // Find element on the left that should be on the right
@@ -54,14 +73,14 @@ public class medianRecursive {
             // Find element on the right that should be on the left
             do {
                 j--;
-            } while (arr[j] > pivot);
+            } while (j > low && arr[j] > pivot);
 
             // If the indices have not crossed, swap elements
             if (i < j) {
                 swap(arr, i, j);
             } else {
                 // Indices have crossed, swap pivot to its correct place
-                swap(arr, i, high - 1);
+                swap(arr, i, high);
                 return i;
             }
         }
@@ -73,3 +92,4 @@ public class medianRecursive {
         arr[j] = temp;
     }
 }
+
